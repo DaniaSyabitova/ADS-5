852f572595380c3877ca4441adfa92fa98b3899a
Dania Syabitova <94595600+DaniaSyabitova@users.noreply.github.com>
2022-04-02 20:32:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,33 +23,32 @@ std::string infx2pstfx(std::string inf) {
     if (priority(inf[i] == 5)) {
       res.push_back(inf[i]); //цифры сразу в строку с пробелом
       res.push_back(' ');
-    }
-    else {
-      if (priority(inf[i] == 4)) {
-        continue; //пропускаем пробел
-      } else if (priority(inf[i] == 0) {
-        stack1.push(inf[i]);
-      } else if (priority(inf[i] > priority(stack1.get())) {
-        stack1.push(inf[i]);
-      } else if (stack1.isEmpty()) {
-        stack1.push(inf[i]);
-      } else if (priority(inf[i] == 1) {
-        while (priority(stack1.get()) != 0) { //до открытой скобки
-          res.push_back(stack1.get());
-          res.push_back(' ');
-          stack1.pop(); //удаление скобки
-        }
-      }
-      else {
-        while ((!stack1.isEmpty()) && (priority(inf[i]) <= priority(stack1.get()))) {
-          res.push_back(stack1.get());
-          res.push_back(' ');
+    } else {
+        if (priority(inf[i] == 4)) {
+          continue; //пропускаем пробел
+        } else if (priority(inf[i] == 0)) {
+          stack1.push(inf[i]);
+        } else if (priority(inf[i] > priority(stack1.get())) {
+          stack1.push(inf[i]);
+        } else if (stack1.isEmpty()) {
+          stack1.push(inf[i]);
+        } else if (priority(inf[i] == 1) {
+          while (priority(stack1.get()) != 0) { //до открытой скобки
+            res.push_back(stack1.get());
+            res.push_back(' ');
+            stack1.pop(); //удаление скобки
+          }
           stack1.pop();
+        } else {
+            while ((!stack1.isEmpty()) && (priority(inf[i]) <= priority(stack1.get()))) {
+              res.push_back(stack1.get());
+              res.push_back(' ');
+              stack1.pop();
+            }
+            stack1.push(inf[i]);
         }
-        stack1.push(inf[i]);
       }
     }
-  }
   while (!stack1.isEmpty()) { //извлечение остатка
     res.push_back(stack1.get());
     res.push_back(' ');
@@ -59,7 +58,7 @@ std::string infx2pstfx(std::string inf) {
   return res;
 }
                  
-int culcul (char op; int x, int y) {
+int culcul (char op, int x, int y) {
   switch (op) {
     case '+': return x + y;
     case '-': return x - y;
@@ -73,8 +72,8 @@ int eval(std::string pref) {
   TStack <char, 100> stack2;
   int res = 0;
   int x, y;
-  for (int i = 0; i < pref.lenght(); i++) {
-    if (priority(pref[i] == 5) {
+  for (int i = 0; i < pref.length(); i++) {
+    if (priority(pref[i] == 5)) {
       stack2.push(pref[i]);
     } else if (priority(pref[i]) < 4) {
       y = stack2.get();
