c804d36328f19cfadd9631b76ec5068e24f77c50
Dania Syabitova <94595600+DaniaSyabitova@users.noreply.github.com>
2022-04-02 20:24:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,87 @@
 #include <map>
 #include "tstack.h"
 
+int priority (char op) {
+    switch (op) {
+      case '(': return 0;
+      case ')': return 1;
+      case '+': return 2;
+      case '-': return 2;
+      case '*': return 3;
+      case '/': return 3;
+      case ' ': return 4;
+      default: return 5; //цифры
+    }
+  }
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string res;
+  TStack <char, 100> stack1;
+  for (int i = 0; i < inf.length(); i++) {
+    if (priority(inf[i] == 5)) {
+      res.push_back(inf[i]); //цифры сразу в строку с пробелом
+      res.push_back(' ');
+    }
+    else {
+      if (priority(inf[i] == 4)) {
+        continue; //пропускаем пробел
+      } else if (priority(inf[i] == 0) {
+        stack1.push(inf[i]);
+      } else if (priority(inf[i] > priority(stack1.get())) {
+        stack1.push(inf[i]);
+      } else if (stack1.isEmpty()) {
+        stack1.push(inf[i]);
+      } else if (priority(inf[i] == 1) {
+        while (priority(stack1.get()) != 0) { //до открытой скобки
+          res.push_back(stack1.get());
+          res.push_back(' ');
+          stack1.pop(); //удаление скобки
+        }
+      }
+      else {
+        while ((!stack1.isEmpty()) && (priority(inf[i]) <= priority(stack1.get()))) {
+          res.push_back(stack1.get());
+          res.push_back(' ');
+          stack1.pop();
+        }
+        stack1.push(inf[i]);
+      }
+    }
+  }
+  while (!stack1.isEmpty()) { //извлечение остатка
+    res.push_back(stack1.get());
+    res.push_back(' ');
+    stack1.pop();
+  }
+  res.pop_back();
+  return res;
+}
+                 
+int culcul (char op; int x, int y) {
+  switch (op) {
+    case '+': return x + y;
+    case '-': return x - y;
+    case '*': return x * y;
+    case '/': return x / y;
+    default: return 0;
+  }
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack <char, 100> stack2;
+  int res = 0;
+  int x, y;
+  for (int i = 0; i < pref.lenght(); i++) {
+    if (priority(pref[i] == 5) {
+      stack2.push(pref[i]);
+    } else if (priority(pref[i]) < 4) {
+      y = stack2.get();
+      stack2.pop();
+      x = stack2.get();
+      stack2.pop();
+      stack2.push(culcul(pref[i], x, y)); //помещаем результат предыдущего действия
+    }
+  }
+  res = stack2.get();
+  return res;
 }
